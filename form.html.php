fef745df7f635ee709dc7aa3c056f19576d1e346
tomas <thomas.a.chilomo@gmail.com>
2018-09-06 21:53:14 -0400

    Bad looking form

---
+++ form.html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>InvoTrend</title>
+    </head>
+    <body>
+        <h1>My Form Practice</h1>
+        <div class='form'>
+            <form action='index.php' method='POST'>
+                <label for='firstname'>First Name:</label>
+                <input type='text' name='firstname' id='firstname'>
+                <br>
+                <label for='lastname'>Last Name:</label>
+                <input type='text' name='lastname' id='lastname'>
+            </form> 
+        </div>
+    </body>
+</html>
